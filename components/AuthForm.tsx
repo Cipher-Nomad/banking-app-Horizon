07f741f4f41a73d4f0ad3100fd53b32e9621ce8b
Akash S G (Cipher-Nomad) <akashsg1398@gmail.com>
2024-06-04 00:20:03 +0530

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter()
@@ -41,8 +42,21 @@ const AuthForm = ({ type }: { type: string }) => {
 
         try {
 
+            const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                ssn: data.ssn!,
+                email: data.email,
+                password: data.password
+            }
+
             if (type === 'sign-up') {
-                const newUser = await signUp(data)
+                const newUser = await signUp(userData)
 
                 setUser(newUser)
             }
@@ -90,7 +104,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </header>
             {user ? (
                 <div className='flex flex-col gap-4'>
-                    {/* PlaidLink */}
+                    <PlaidLink user={user} variant="primary"></PlaidLink>
                 </div>
             ) :
                 <>
