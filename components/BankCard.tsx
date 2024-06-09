60cecc947256bf5ad0d45d8f77b3d6c1eb5c0265
Akash S G (Cipher-Nomad) <akashsg1398@gmail.com>
2024-06-09 21:03:33 +0530

    finalize the app

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,11 +2,12 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
     return (
         <div className='flex flex-col'>
-            <Link href="/" className='bank-card'>
+            <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className='bank-card'>
                 <div className='bank-card_content'>
                     <div>
                         <h1 className='text-16 font-semibold text-white'>
@@ -20,7 +21,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     <article className='flex flex-col gap-2'>
                         <div className='flex justify-between'>
                             <h1 className='text-12 font-semibold text-white'>
-                                {userName}
+                                {account.name}
                             </h1>
                             <h2 className='text-12 font-semibold'>
                                 ●● / ●●
@@ -28,7 +29,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                         </div>
 
                         <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                            ●●●● ●●●● ●●●● <span className='text-16'>1234</span>
+                            ●●●● ●●●● ●●●● <span className='text-16'>{account?.mask}</span>
                         </p>
                     </article>
                 </div>
@@ -56,6 +57,8 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     alt='lines'
                     className='absolute top-0 left-0' />
             </Link>
+
+            {showBalance && <Copy title={account?.shareableId} />}
         </div>
     )
 }
