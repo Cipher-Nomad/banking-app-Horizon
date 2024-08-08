8cea4a805dd04becd5b8a90e55d0aa5c8a81f4a5
Akash S G (Cipher-Nomad) <akashsg1398@gmail.com>
2024-08-08 11:17:10 +0530

    add extra notes

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,6 +4,8 @@ const SignIn = () => {
     return (
         <section className="flex-center size-full max-sm:px-6">
             <AuthForm type="sign-in" />
+            <div className='absolute left-0 top-0'>testing email: <strong>dummyUser2@dummy.com</strong> <br />
+                testing password: <strong>dummy123</strong></div>
         </section>
     )
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,6 +3,8 @@ import AuthForm from '@/components/AuthForm'
 const SignUp = async () => {
     return (
         <section className="flex-center size-full max-sm:px-6">
+            <div className='flex absolute left-0 top-0'><p>needs a valid 2-letter <strong>State</strong>, Dwolla is US based authenticator. Enter "<strong><em>NY</em></strong>"
+                or any from this <a href='https://www23.statcan.gc.ca/imdb/p3VD.pl?Function=getVD&TVD=53971' target="_blank"><strong><u>states</u></strong></a></p></div>
             <AuthForm type="sign-up" />
         </section>
     )
